f62b911644360fa24c54cc8f216183cb0ce961a1
kirna <assshh351@gmail.com>
2024-09-16 00:22:53 -0700

    pruebas

---
+++ src/main/java/kirna/proyectoclub/models/usuario.java
@@ -0,0 +1,44 @@
+package kirna.proyectoclub.models;
+
+public class usuario {
+
+    private String nombre;
+    private String email;
+    private int edad;
+
+    // Constructor vacío
+    public usuario() {}
+
+    // Constructor con parámetros
+    public usuario(String nombre, String email, int edad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
